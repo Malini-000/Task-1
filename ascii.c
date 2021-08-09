7e5833dc75345ca8038a4a3c49dbcbde5789ece7
Malini-000 <82525618+Malini-000@users.noreply.github.com>
2021-08-09 20:13:58 +0530

    Add files via upload

---
+++ ascii.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+char s[10];
+int i;
+
+
+printf("Enter the string: ");
+gets(s);
+while (s[i])
+{
+
+printf("the string is %c",s);
+printf("the string is %d",s);
+i++;
+
+}
+}
+
